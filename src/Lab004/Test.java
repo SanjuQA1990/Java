fad94db57d6c6ec94d13d9ba6dfaa4148aba6a1b
sanjay <sanjaykumar.emmbros@gmail.com>
2024-10-04 07:27:21 +0530

    New Program added

--- src/Lab004/Test.java
+++ src/Lab004/Test.java
@@ -3,6 +3,6 @@ package Lab004;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Good progrma");
+        System.out.println("Good Program");
     }
 }
