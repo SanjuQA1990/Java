2d58518444f638ed04ecaab431ec982710bdbe3d
sanjay <sanjaykumar.emmbros@gmail.com>
2024-10-04 07:10:27 +0530

    All well

---
+++ src/Lab001/Lab001.java
@@ -0,0 +1,7 @@
+package Lab001;
+
+public class Lab001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ src/Lab002/Lab002.java
@@ -0,0 +1,7 @@
+package Lab002;
+
+public class Lab002 {
+    public static void main(String[] args) {
+        System.out.println("My First Program");
+    }
+}
