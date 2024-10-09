2ed0711760aae0ac98dc78da70c90c00fa5e2037
sanjay <sanjaykumar.emmbros@gmail.com>
2024-10-09 22:49:25 +0530

    Added new program for table using the printf() in java

---
+++ src/EX_09_10_24/Lab006.java
@@ -0,0 +1,19 @@
+package EX_09_10_24;
+
+public class Lab006 {
+    public static void main(String[] args) {
+
+        String name = "Manoj";        // String example
+        int id = 12542585;              // Integer example
+        double price = 20411.25;           // Floating-point example
+
+        // Using %s for strings
+        System.out.printf("name: %s\n", name);
+
+        // Using %d for integers
+        System.out.printf("id: %d\n", id);
+
+        // Using %.2f for floating-point numbers with 2 decimal places
+        System.out.printf("Price: %.2f\n", price);
+    }
+}
