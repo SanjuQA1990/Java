9cb47ad27ef250be8116fa9aaafc5e3abcc842a2
sanjay <sanjaykumar.emmbros@gmail.com>
2024-10-09 22:47:13 +0530

    Added new program for constant in Java

---
+++ src/EX_09_10_24/Lab007.java
@@ -0,0 +1,18 @@
+package EX_09_10_24;
+
+public class Lab007 {
+    public static void main(String[] args) {
+        int item=5;
+       // System.out.printf("%d %d %d",  item);
+        System.out.printf("%d * %d = %d \n",item,1,item*1);
+        System.out.printf("%d * %d = %d \n",item,2,item*2);
+        System.out.printf("%d * %d = %d \n",item,3,item*3);
+        System.out.printf("%d * %d = %d \n",item,4,item*4);
+        System.out.printf("%d * %d = %d \n",item,5,item*5);
+        System.out.printf("%d * %d = %d \n",item,6,item*6);
+        System.out.printf("%d * %d = %d \n",item,7,item*7);
+        System.out.printf("%d * %d = %d \n",item,8,item*8);
+        System.out.printf("%d * %d = %d \n",item,9,item*9);
+        System.out.printf("%d * %d = %d \n",item,10,item*10);
+    }
+}
