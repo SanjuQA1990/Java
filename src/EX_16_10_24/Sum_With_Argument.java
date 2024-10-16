84e55a77650bc652c14c091b02db92f45426c13c
sanjay <sanjaykumar.emmbros@gmail.com>
2024-10-16 21:48:37 +0530

    16 Oct Max of 2 number using argument & User input with Scanner class

---
+++ src/EX_16_10_24/Sum_With_Argument.java
@@ -0,0 +1,14 @@
+package EX_16_10_24;
+
+public class Sum_With_Argument {
+    public static void main(String[] args) {
+
+        int a =Integer.parseInt(args[0]); // 80
+        int b =Integer.parseInt(args[1]); // 156
+
+        int result= a+b;
+
+        System.out.println("Sum of a + b is==> " +result );
+
+    }
+}
